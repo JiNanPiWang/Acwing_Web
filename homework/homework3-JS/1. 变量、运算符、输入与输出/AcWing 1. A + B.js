3228aaebb91b5759b1cb59adbd3dcc7d4d05ec99
G3 <purep@qq.com>
2024-02-05 20:48:35 +0800

    完成js1.1

--- homework/homework3-JS/1. 变量、运算符、输入与输出/AcWing 1. A + B.js
+++ homework/homework3-JS/1. 变量、运算符、输入与输出/AcWing 1. A + B.js
@@ -1,1 +1,11 @@
-a=1
+let buf = "";
+
+process.stdin.on("readable", function() {
+    let chunk = process.stdin.read();
+    if (chunk) buf += chunk.toString();
+});
+
+process.stdin.on("end", function() {
+    let [a, b] = buf.split(' ').map(Number);
+    console.log(a + b);
+});
